cbf0eadff3a3923863bc59fed83a283c8cdc271c
Navya21031995 <sampathnavya@gmail.com>
2023-02-24 14:23:36 +0530

    Code changes

---
+++ src/test/java/TestPackage/TestClass.java
@@ -0,0 +1,22 @@
+package TestPackage;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeSuite;
+
+public class TestClass {
+	
+	@Test
+	public void testcase01() {
+		System.out.println( " Hello TC01");
+	}
+
+	@Test
+	public void testcase02() {
+		System.out.println( " Hello TC02");
+	}
+
+	@Test
+	public void testcase03() {
+		System.out.println( " Hello TC03");
+	}
+	
+}
